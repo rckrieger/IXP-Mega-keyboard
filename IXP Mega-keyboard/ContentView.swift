109ed0c2f1e00dd0ba2d843a1480045577788a7c
rckrieger <rebecca.c.krieger@gmail.com>
2021-04-21 07:36:52 -0700

    Initial Commit

---
+++ IXP Mega-keyboard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IXP Mega-keyboard
+//
+//  Created by Rebecca Krieger on 4/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
